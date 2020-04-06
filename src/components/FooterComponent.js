06d93a1b99746540c935d03cd8d655050f0b1b64
Adi Nomberg <adinomberg6@gmail.com>
2020-04-06 18:51:16 +0300

    added footer and itinerary form

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div class="container"></div>
+            <div className="footer">
+                <div className="main-footer">
+                    <div className="footer-col col1">
+                        <h2 className="footer-headline">MyTrip</h2>
+                        <p> A free online trip planner that can help you build a personalized itinerary for your next vacation. </p>
+                    </div>
+                    <div className="footer-col col2">
+                        <ul className="list-footer">
+                            <li className=""><h2 className="footer-headline">Information</h2></li>
+                            <li className=""><Link className="footer-link" to="/about">About</Link></li>
+                            <li className=""><Link className="footer-link" to="/contact">Contact Us</Link></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col col3">
+                        <ul className="list-footer">
+                            <li className=""><h2 className="footer-headline">Have a Question?</h2></li>
+                            <li><i className="fa fa-map-marker"></i><span class="footer-text"> 1 Ben-Gurion Ave, Beer-Sheva, Israel</span></li>
+                            <li><i className="fa fa-phone"></i><span class="footer-text"> +972-8-646-1600</span></li>
+                            <li><i className="fa fa-envelope"></i><span class="footer-text"> info@mytrip.com</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="social-footer">
+                    <ul class="social-footer-list">
+                        <li><a href="https://www.facebook.com/adi.nomberg" target="_blank" rel="noopener noreferrer"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/adi_nomberg" target="_blank" rel="noopener noreferrer"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="https://github.com/adinomberg" target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/adinomberg/" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+                <div className="copyright-footer">
+                    <ul className="copyright-footer-list">
+                        <li><i className="fa fa-copyright"></i> 2020 Copyright MyTrip</li>
+                        <li>This website is made with <i class="fa fa-heart"></i> by Adi Nomberg</li>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import '../styles/Home.css';
 
+
 const Home = () => {
     return (
         <div className="home">
             <div className="background">
                 <div className="container">
                     <div className="title">
-                        <h2 className="mytrip-title">MyTrip</h2>
+                        <h2 className="mytrip-title">Plan the trip
+                            of your dreams</h2>
                         <p>MyTrip is the new way to plan your next trip!</p>
+                    </div>
+                    <div className="form-container">
+                        <div className="form-title">
+                            Itinerary Planner
+                        </div>
+                        <form>
+                            <input list="destination-of-trip" className="destination-input" name="Destination of Trip" placeholder="Enter destination (Country)" />
+                            <datalist id="destination-of-trip">
+                                <option value="Worldwide" selected></option>
+                                <option value="Australia"></option>
+                                <option value="Belgium"></option>
+                                <option value="Canada"></option>
+                                <option value="Denmark"></option>
+                                <option value="Egypt"></option>
+                                <option value="France"></option>
+                                <option value="Greece"></option>
+                                <option value="Hungary"></option>
+                                <option value="Israel"></option>
+                                <option value="Japan"></option>
+                                <option value="Korea"></option>
+                                <option value="Latvia"></option>
+                                <option value="Mexico"></option>
+                                <option value="New Zealand"></option>
+                                <option value="Oman"></option>
+                                <option value="Peru"></option>
+                                <option value="Qatar"></option>
+                                <option value="Russia"></option>
+                                <option value="Spain"></option>
+                                <option value="Tanzania"></option>
+                                <option value="United States"></option>
+                                <option value="Vietnam"></option>
+                                <option value="Yemen"></option>
+                                <option value="Zimbabwe"></option>&nbsp;
+                            </datalist>
+                            <div className="start-date">
+                                <label for="start-date"> Start: </label>
+                                <input id="start-date" type="date" className="start-date-input" />
+                            </div>
+                            <div className="end-date">
+                                <label for="end-date"> End: </label>
+                                <input id="end-date" type="date" className="end-date-input" />
+                            </div>
+                            <button type="submit" name="form-submit" className="form-button">Start planning your trip</button>
+                        </form>
                     </div>
                 </div>
             </div>
